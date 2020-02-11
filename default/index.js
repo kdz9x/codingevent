f5b25ed040866255f020e4703337e9d6d1f3b88a
kdz9x <khanh.kdz9x@gmail.com>
2020-02-11 15:08:14 +0700

    khanh

--- default/index.js
+++ default/index.js
@@ -1,10 +1,11 @@
 function gettext(){
-    var a = ("");
-    let number = a.replace(",","");
+    var a = ("10,20");
+    let number = a.replace(","," ");
+    let newNumber = number.split(" ")
     const total = () => {
         var sum=0;
-        for (var i = 0;i<number.length;i++){
-            sum += Number(number[i]);
+        for (var i = 0;i<newNumber.length;i++){
+            sum += Number(newNumber[i]);
         }
         console.log(sum)
     }
