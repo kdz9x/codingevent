0eb069054376c48d4e9b1687021ae6a80c0727b7
kdz9x <khanh.kdz9x@gmail.com>
2020-02-12 10:09:45 +0700

    khanh

--- adv3/index.js
+++ adv3/index.js
@@ -1,9 +1,9 @@
 function gettext(){
-    var j =(";");//Dinh nghia ki tu phan chia
-    var a = ("//" + j + "\n1;3"); //nhap ki tu
-    console.log(a);
+    var j =("-");//Dinh nghia ki tu phan chia
+    var a = ("//" + j + "\n1-3-4"); //nhap ki tu
+    var result = "\\" + j;
     let b = a.slice('4');
-    let number = b.replace(j," ");
+    let number = b.replace(new RegExp(result,'g')," ");
     let newNumber = number.split(" ");
     function total() {
         var sum=0;
